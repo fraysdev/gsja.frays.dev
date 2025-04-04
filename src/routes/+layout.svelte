e8c6dc64a42ac0422d1018c12e006a5e4867ff79
FrankyRay <frankyraymarcellsinaga@gmail.com>
2025-04-04 18:17:28 +0700

    chg: Add metadata to pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <svelte:head>
     <title>Home | gsja.frays.dev</title>
+    <meta name="description" content="Welcome to gsja.frays.dev! This subdomain used for project related to GSJA only!">
+    <meta name="author" content="FrankyRayMS">
+    <meta name="keywords" content="website, gsja, gsja betlehem, lifekids">
     <link rel="shortcut icon" href="/favicon.png" type="image/x-icon">
 </svelte:head>
 
--- src/routes/lk/+layout.svelte
+++ src/routes/lk/+layout.svelte
@@ -1,5 +1,6 @@
 <svelte:head>
-    <title>Doorprize Life Kids | gsja.frays.dev</title>
+    <title>Doorprize LIFEKids | gsja.frays.dev</title>
+    <meta name="description" content="Doorprize LIFEKids! Menampilkan sebuah RNG (Random Number Generated; Nomor Acak) yang digunakan sebagai nomor undian doorprize pada acara besar LIFEKids GSJA Betlehem. Tema yang digunakan merujuk pada Paskah LIFEKids 2025.">
     <link rel="shortcut icon" href="/lifekids.png" type="image/x-icon">
 </svelte:head>
 
