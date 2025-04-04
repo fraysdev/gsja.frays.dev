5f5253a27a3253e95d1b45fe0ed9a119a5d6ceab
FrankyRay <frankyraymarcellsinaga@gmail.com>
2025-04-04 17:17:26 +0700

    chg: Change home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,61 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div id="app">
+    <h1>Welcome to <code>gsja.frays.dev</code>!</h1>
+    <p>This subdomain used for project related to GSJA only!</p>
+
+    <div id="info">
+        This web use 
+        <a href="https://svelte.dev/">
+            <img class="svelte-icon" src="/svelte.png" alt="Svelte">
+            <span class="svelte">Svelte</span>
+        </a>
+        as Web Framework!
+    </div>
+</div>
+
+<!-- -+---+- Styles -+---+- -->
+<style>
+    :global(body) {
+        margin: 0;
+    }
+
+    #app {
+        height: 100vh;
+        background-color: antiquewhite;
+
+        font-family: "Lexend", sans-serif;
+        font-optical-sizing: auto;
+        
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #info {
+        position: absolute;
+        right: 1vh;
+        bottom: 1vh;
+    }
+
+    .svelte-icon {
+        height: 1em;
+    }
+
+    .svelte {
+        color: #ff3e00;
+    }
+
+    code {
+        font-family: "IBM Plex Mono", monospace;
+        font-style: normal;
+    }
+
+    h1, p {
+        margin: 0 0;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+</style>
